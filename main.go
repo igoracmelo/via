65b805f2cfa0d9307eb8a01847ea1b8f6ad8a086
Igor Melo <igoracmelo@protonmail.com>
2023-02-18 19:58:32 -0300

    adiciona cor para ramal saracuruna

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ var colors = map[string]string{
 	"santa_cruz": "\033[0;32m",
 	"paracambi":  "\033[0;36m",
 	"japeri":     "\033[0;34m",
+	"saracuruna": "\033[0;33m",
 	"bwhite":     "\033[1m",
 }
 
