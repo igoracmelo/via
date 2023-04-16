45702c0cdd6c85d6aa1d0d3d2e599915e8f26a93
Igor <igoracmelo@protonmail.com>
2023-04-16 17:28:27 -0300

    diversas modificações

--- main.go
+++ main.go
@@ -5,10 +5,14 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"log"
+	"math"
 	"net/http"
 	"net/url"
 	"os"
 	"path"
+	"regexp"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -25,111 +29,213 @@ func color(s string, name string) string {
 	return colors[name] + s + "\033[0m"
 }
 
-func main() {
-	flag.Parse()
+var planDay string
+var planTime string
 
-	if flag.NArg() < 1 {
-		panic("argumentos insuficientes") // TODO:
+func main() {
+	pCmd := flag.NewFlagSet("p", flag.ExitOnError)
+	pCmd.StringVar(&planTime, "t", "", "hora da viagem")
+	pCmd.StringVar(&planDay, "d", "", "dia da viagem")
+
+	if len(os.Args) < 1 {
+		log.Panic("argumentos insuficientes")
 	}
 
-	if flag.Arg(0) == "p" {
-		var (
-			from  string
-			to    string
-			sdate string
-			stime string
-		)
-
-		if flag.NArg() < 3 {
-			panic("argumentos insuficientes") // TODO:
+	switch os.Args[1] {
+	case "p":
+		if len(os.Args) < 4 {
+			log.Panic("argumentos insuficientes")
 		}
 
-		stations, err := getStationsCached()
-		if err != nil {
-			panic(err) // TODO:
-		}
-
-		from = findStationBestMatch(flag.Arg(1), stations)
-		to = findStationBestMatch(flag.Arg(2), stations)
-
-		if flag.NArg() == 3 {
-			now := time.Now().Add(2 * time.Minute)
-			sdate = now.Format("2006-01-02")
-			stime = now.Format("15:04")
-
-			fmt.Printf("Planejando para daqui há 2 minutos (%s -> %s)\n\n", from, to)
-		}
-
-		if flag.NArg() == 4 {
-			stime = flag.Arg(3)
-			now := time.Now().Add(2 * time.Minute)
-			sdate = now.Format("2006-01-02")
-
-			fmt.Printf("Planejando para hoje as %s (%s -> %s)\n\n", stime, from, to)
-		}
-
-		if flag.NArg() == 5 {
-			stime = flag.Arg(3)
-			splits := strings.Split(os.Args[5], "/")
-			d := splits[0]
-
-			m := ""
-			if len(splits) > 1 {
-				m = splits[1]
-			} else {
-				m = time.Now().Format("01")
+		if len(os.Args) > 4 {
+			err := pCmd.Parse(os.Args[4:])
+			if err != nil {
+				log.Panic(err)
 			}
-
-			y := ""
-			if len(splits) > 2 {
-				y = splits[2]
-			} else {
-				y = time.Now().Format("2006")
-			}
-
-			sdate = fmt.Sprintf("%s-%s-%s", y, m, d)
-			fmt.Printf("Planejando para dia %s/%s/%s as %s (%s -> %s)\n\n", d, m, y, stime, from, to)
 		}
 
-		_, err = getAlerts(to, from, sdate, stime)
+		stations, err := getStations()
 		if err != nil {
-			panic(err) // TODO:
+			log.Panic(err)
 		}
 
-		plan, err := getTripPlan(from, to, sdate, stime)
+		from := findStationBestMatch(os.Args[2], stations)
+		to := findStationBestMatch(os.Args[3], stations)
+
+		now := time.Now().Add(2 * time.Minute)
+
+		if planDay == "" {
+			planDay = now.Format("2006-01-02")
+		} else {
+			planDay = formatPlanDay(planDay, now)
+		}
+
+		if planTime == "" {
+			planTime = now.Format("15:04")
+		} else {
+			planTime = formatPlanTime(planTime, now)
+		}
+
+		_, err = getAlerts(to, from, planDay, planTime)
 		if err != nil {
-			panic(err) // TODO:
+			log.Panic(err)
 		}
 
-		for i, traject := range plan.Trajects {
-			fmt.Println(color("Trajeto", "bwhite"), i+1)
-			fmt.Println()
+		t, err := time.Parse("2006-01-02 15:04", planDay+" "+planTime)
+		if err != nil {
+			log.Panic(err)
+		}
 
-			for j, trip := range traject.Trips {
-				fmt.Println("> Opção", j+1)
+		fmt.Printf("planejando para %s\n\n", humanReadableTime(t))
 
-				for _, subtrip := range trip {
-					fmt.Printf("%s - %s\n", subtrip.TimeDeparture[:5], color(subtrip.StationNameOrigin, "bwhite"))
-					fmt.Println(color("          |", subtrip.ExtensionId))
+		plan, err := getTripPlan(from, to, planDay, planTime)
+		if err != nil {
+			log.Panic(err)
+		}
+
+		printTrajects(plan)
+	}
+}
+
+func humanReadableTime(t time.Time) string {
+	_, offset := time.Now().Zone()
+	now := time.Now().UTC().Add(time.Duration(offset) * time.Second)
+	today := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.Local)
+	tomorrow := today.Add(24 * time.Hour)
+	afterTomorrow := tomorrow.Add(24 * time.Hour)
+	in30min := now.Add(30 * time.Minute)
+
+	s := ""
+
+	if t.After(today) && t.Before(tomorrow) {
+		s += "hoje "
+	} else if t.After(tomorrow) && t.Before(afterTomorrow) {
+		s += "amanhã "
+	} else if t.Month() == now.Month() && t.Year() == now.Year() {
+		s += "dia " + t.Format("02") + " "
+	} else if t.Year() == now.Year() {
+		s += "dia " + t.Format("02/01") + " "
+	} else {
+		s += "dia " + t.Format("02/01/06") + " "
+	}
+
+	if t.After(now) && t.Before(in30min) {
+		mins := int(math.Floor(t.Sub(now).Minutes()))
+		if mins == 0 {
+			s += "agora"
+		} else if mins == 1 {
+			s += "em " + fmt.Sprint(mins) + " minuto"
+		} else {
+			s += "em " + fmt.Sprint(mins) + " minutos"
+		}
+	} else {
+		s += "as " + t.Format("15:04") + " horas"
+	}
+
+	return s
+}
+
+func printTrajects(plan *TripPlanResponse) {
+	for i, traject := range plan.Trajects {
+		fmt.Println(color("Trajeto", "bwhite"), i+1)
+		fmt.Println()
+
+		for j, trip := range traject.Trips {
+			fmt.Println("> Opção", j+1)
+
+			for _, subtrip := range trip {
+				dep := strings.Repeat(" ", 7)
+				if len(subtrip.TimeDeparture) > 5 {
+					dep = subtrip.TimeDeparture[:5] + " - "
 				}
-
-				last := trip[len(trip)-1]
-				fmt.Printf("%s - %s\n", last.TimeArrival[:5], color(last.StationNameDest, "bwhite"))
-
-				fmt.Println()
-				fmt.Println()
+				fmt.Println(dep + color(subtrip.StationNameOrigin, "bwhite"))
+				fmt.Println(color("          |", subtrip.ExtensionId))
 			}
+
+			last := trip[len(trip)-1]
+			fmt.Printf("%s - %s\n", last.TimeArrival[:5], color(last.StationNameDest, "bwhite"))
+
+			fmt.Println()
+			fmt.Println()
 		}
 	}
 }
 
+func formatPlanTime(s string, fallback time.Time) string {
+	if regexp.MustCompile(`^\d{2}:\d{2}`).MatchString(s) {
+		return s
+	}
+
+	n, err := strconv.Atoi(s)
+	if err != nil {
+		return fallback.Format("15:04")
+	}
+
+	if n < 10 {
+		s = "0" + s
+	}
+
+	s += ":00"
+	return s
+}
+
+func formatPlanDay(s string, fallback time.Time) string {
+	splits := strings.Split(s, "/")
+	d := splits[0]
+
+	m := ""
+	if len(splits) > 1 {
+		m = splits[1]
+	} else {
+		m = fallback.Format("01")
+	}
+
+	y := ""
+	if len(splits) > 2 {
+		y = splits[2]
+	} else {
+		y = fallback.Format("2006")
+	}
+
+	return fmt.Sprintf("%s-%s-%s", y, m, d)
+}
+
 func getStations() (*StationsResponse, error) {
-	resp, err := http.Get("https://content.supervia.com.br/estacoes")
+	stations, err := getStationsCache()
+	if err == nil {
+		return stations, nil
+	}
+
+	stations, err = getStationsOnline()
+	if err != nil {
+		return nil, err
+	}
+
+	err = storeStationsCache(stations)
+	if err != nil {
+		log.Print(err)
+	}
+
+	return stations, nil
+}
+
+func getStationsOnline() (*StationsResponse, error) {
+	req, err := http.NewRequest("GET", "https://content.supervia.com.br/estacoes", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	printRequest(req)
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
 
 	body, err := io.ReadAll(resp.Body)
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("status: %d\nbody: %s", resp.StatusCode, string(body))
+	}
+
 	if err != nil {
 		return nil, err
 	}
@@ -139,42 +245,57 @@ func getStations() (*StationsResponse, error) {
 	return stations, err
 }
 
-func getStationsCached() (*StationsResponse, error) {
+func getStationsCache() (*StationsResponse, error) {
 	p := path.Join(os.TempDir(), "via-stations-cache")
 	stations := &StationsResponse{}
 
 	data, err := os.ReadFile(p)
-	if err == nil {
-		err = json.Unmarshal(data, stations)
-
-		// unmarshal may fail because the file can be empty or corrupted
-		if err == nil {
-			return stations, nil
-		}
-	}
-
-	stations, err = getStations()
 	if err != nil {
 		return nil, err
 	}
 
-	// caching is optional, so these errors should not be returned
-	data, err = json.Marshal(stations)
-	if err == nil {
-		_ = os.WriteFile(p, data, 0666)
+	err = json.Unmarshal(data, stations)
+	if err != nil {
+		return nil, err
 	}
 
 	return stations, nil
 }
 
+func storeStationsCache(stations *StationsResponse) error {
+	p := path.Join(os.TempDir(), "via-stations-cache")
+
+	data, err := json.Marshal(stations)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(p, data, 0666)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func getTripPlan(from string, to string, sdate string, stime string) (*TripPlanResponse, error) {
 	url := fmt.Sprintf("https://content.supervia.com.br/planeje/%s/%s/%s/%s", from, to, sdate, stime)
-	resp, err := http.Get(url)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	printRequest(req)
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
 
 	body, err := io.ReadAll(resp.Body)
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("status: %d\nbody: %s", resp.StatusCode, string(body))
+	}
 	if err != nil {
 		return nil, err
 	}
@@ -219,12 +340,16 @@ func getAlerts(to, from, sdate, stime string) (*AlertsResponse, error) {
 		},
 	}
 
+	printRequest(req)
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
 
 	body, err := io.ReadAll(resp.Body)
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("status: %d\nbody: %s", resp.StatusCode, string(body))
+	}
 	if err != nil {
 		return nil, err
 	}
@@ -236,6 +361,10 @@ func getAlerts(to, from, sdate, stime string) (*AlertsResponse, error) {
 	}
 
 	return alerts, nil
+}
+
+func printRequest(req *http.Request) {
+	// fmt.Println(req.Method, req.URL.String())
 }
 
 func findStationBestMatch(station string, stations *StationsResponse) string {
